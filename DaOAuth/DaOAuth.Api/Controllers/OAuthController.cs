c72cfd242a15a35fe592dba1fb6dae33e4ea0718
David André <dev.david.andre@gmail.com>
2018-04-17 20:58:23 +0200

    Commit initial

---
+++ DaOAuth/DaOAuth.Api/Controllers/OAuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DaOAuth.Api.Controllers
+{
+    /// <summary>
+    /// Controller implémentant les endpoints d'un serveur d'authorisation OAuth2 
+    /// </summary>
+    public class OAuthController : ApiController
+    {
+        
+    }
+}
