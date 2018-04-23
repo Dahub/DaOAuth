e80675884175a2e170f7b0002dae133b53e37981
Dahub <dev.david.andre@gmail.com>
2018-04-23 22:02:50 +0200

    authorize : error via redirection

--- DaOAuth/DaOAuth.Api/Controllers/v1.0/OAuthController.cs
+++ DaOAuth/DaOAuth.Api/Controllers/v1.0/OAuthController.cs
@@ -27,64 +27,94 @@ namespace DaOAuth.Api.Controllers.V1_0
                 sont attendus : "response_type" , "client_id"
                 en optionel : "redirect_uri", "scope"
                 en recommandé : "state" */
+                bool isError = false;
+                string errorMsg = String.Empty;
 
                 // state
                 string myState = String.Empty;
                 if (!TryExtractUniqueRequestParamValue("state", false, out myState))
-                    return GenerateErrorResponse("invalid_request", "Le paramètre state doit être présent une et une seule fois");
+                {
+                    errorMsg = GenerateErrorMessage("invalid_request", "Le paramètre state doit être présent une et une seule fois");
+                    isError = true;
+                }
 
                 // response type
                 string response_type = String.Empty;
-                if (!TryExtractUniqueRequestParamValue("response_type", true, out response_type))
-                    return GenerateErrorResponse("invalid_request", "Le paramètre response_type doit être présent une et une seule fois et avoir une valeur", myState);
-                if (response_type != "code")
-                    return GenerateErrorResponse("unsupported_response_type", "La valeur du paramètre response_type doit être code", myState);
+                if (!isError && !TryExtractUniqueRequestParamValue("response_type", true, out response_type))
+                {
+                    errorMsg = GenerateErrorMessage("invalid_request", "Le paramètre response_type doit être présent une et une seule fois et avoir une valeur", myState);
+                    isError = true;
+                }
+                if (!isError && response_type != "code")
+                {
+                    errorMsg = GenerateErrorMessage("unsupported_response_type", "La valeur du paramètre response_type doit être code", myState);
+                    isError = true;
+                }
 
                 // client_id
                 string client_id = String.Empty;
                 if (!TryExtractUniqueRequestParamValue("client_id", true, out client_id))
-                    return GenerateErrorResponse("invalid_request", "Le paramètre client_id doit être présent une et une seule fois et avoir une valeur", myState);
+                {
+                    errorMsg = GenerateErrorMessage("invalid_request", "Le paramètre client_id doit être présent une et une seule fois et avoir une valeur", myState);
+                    isError = true;
+                }
 
                 // redirect url
                 string redirectUri = String.Empty;
                 if (!TryExtractUniqueRequestParamValue("redirect_uri", false, out redirectUri))
-                    return GenerateErrorResponse("invalid_request", "Le paramètre client_id doit être présent une et une seule fois et avoir une valeur", myState);
-                
+                { 
+                    errorMsg = GenerateErrorMessage("invalid_request", "Le paramètre redirect_uri doit être présent une et une seule fois et avoir une valeur", myState);
+                    isError = true;
+                }
+
                 ClientService cs = new ClientService()
                 {
                     ConnexionString = ConfigurationWrapper.Instance.ConnexionString,
                     Factory = new EfRepositoriesFactory()
                 };
-                var clientInfos = cs.GetClientInfoForAuthorizationCodeGrant(client_id, redirectUri);
 
+                var clientInfos = cs.GetClientInfoForAuthorizationCodeGrant(client_id, redirectUri);
                 if (!clientInfos.IsValid)
-                    return GenerateErrorResponse("unauthorized_client", "Le client ne possède pas les droits de demander une authorisation", myState);
+                { 
+                    errorMsg = GenerateErrorMessage("unauthorized_client", "Le client ne possède pas les droits de demander une authorisation", myState);
+                    isError = true;
+                }
+
+                string location = String.Empty;
 
                 // tout est ok, on peut générer un nouveau code pour cette demande
-                var myCode = cs.AddCodeToClient(client_id);
-
-                string location = String.Concat(clientInfos.RedirectUri, "?code=", myCode.CodeValue);
-                if (!String.IsNullOrEmpty(myState))
-                    location = String.Concat(location, "&state=", myState);
+                if (!isError)
+                {
+                    var myCode = cs.AddCodeToClient(client_id);
+                    location = String.Concat(clientInfos.RedirectUri, "?code=", myCode.CodeValue);
+                    if (!String.IsNullOrEmpty(myState))
+                        location = String.Concat(location, "&state=", myState);
+                }
+                else
+                {
+                    location = String.Concat(clientInfos.IsValid?clientInfos.RedirectUri:redirectUri, "?", errorMsg);
+                }               
 
                 // Création de la response de redirection
                 var response = Request.CreateResponse(HttpStatusCode.Moved);
                 Uri myUri;
                 if (!Uri.TryCreate(location, UriKind.Absolute, out myUri))
-                    return GenerateErrorResponse("invalid_request", "l'adresse de redirection est invalide", myState);
+                    return BadRequest("l'adresse de redirection est invalide"); // seul cas ou on redirige pas : aucune adresse de redirection
                 response.Headers.Location = myUri;
 
                 return ResponseMessage(response);
             }
             catch (Exception ex)
             {
-                /* The authorization server encountered an unexpected
-                condition that prevented it from fulfilling the request.
-                (This error code is needed because a 500 Internal Server
-                Error HTTP status code cannot be returned to the client
-                via an HTTP redirect.) */
-                return GenerateErrorResponse("server_error", String.Format("Une erreur s'est produite : {0}", ex.Message));
+                return InternalServerError(ex);
             }
+        }
+
+        [HttpPost]
+        [Route("token")]
+        public IHttpActionResult Token()
+        {
+            return Ok();
         }
 
         private bool TryExtractUniqueRequestParamValue(string paramName, bool mustExist, out string value)
@@ -105,30 +135,17 @@ namespace DaOAuth.Api.Controllers.V1_0
             return true;
         }
 
-        private IHttpActionResult GenerateErrorResponse(string errorName, string errorDescription, string stateInfo)
+        private string GenerateErrorMessage(string errorName, string errorDescription, string stateInfo)
         {
             if (String.IsNullOrEmpty(stateInfo))
-                return GenerateErrorResponse(errorName, errorDescription);
+                return GenerateErrorMessage(errorName, errorDescription);
 
-            var myError = new
-            {
-                error = errorName,
-                error_description = errorDescription,
-                state = stateInfo
-            };
-
-            return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, myError));
+            return String.Format("error={0}&error_description={1}&state={2}", errorName, errorDescription, stateInfo);
         }
 
-        private IHttpActionResult GenerateErrorResponse(string errorName, string errorDescription)
+        private string GenerateErrorMessage(string errorName, string errorDescription)
         {
-            var myError = new
-            {
-                error = errorName,
-                error_description = errorDescription
-            };
-
-            return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, myError));
+            return String.Format("error={0}&error_description={1}", errorName, errorDescription);
         }
     }
 }
